92fa949ec78ff363ec63b7915ea7127171cc12d5
abigailkeller <abigail.g.keller1@gmail.com>
2024-12-16 23:04:44 +0000

    created model data

---
+++ code/seasonal_growth.R
@@ -0,0 +1,101 @@
+library(tidyverse)
+library(nimble)
+library(MCMCvis)
+
+# read in data
+data <- readRDS('data/model_data/growth_data.rds')
+
+# nimble code
+seasonal_growth <- nimbleCode({
+  # prior distributions
+  k ~ dunif(0,2) # growth rate
+  C ~ dunif(0,2) # size of seasonal variation
+  ts ~ dunif(-1,0) # time between t=0 and start of growth oscillation
+  t0 ~ dunif(-1,1) # age organism has 0 size
+  tau.y ~ dunif(0,100) # process error sd
+  tau.ranef ~ dunif(0,100) # year random effect sd
+  size_inf ~ dunif(80,140) # 
+  
+  for(i in 1:nsizes){
+    size[i] ~ dnorm(y.hat[i], tau.y)
+    y.hat[i] <- size_inf*(1-exp(-k*(t[i]-t0) - S_t[i] + S_t0)) + ranef[year[i]]
+    S_t[i] <- (C*k/(2*pi))*sin(2*pi*(t[i]-ts))
+  }
+  S_t0 <- (C*k/(2*pi))*sin(2*pi*(t0-ts))
+  
+  for(y in 1:nyears){
+    ranef[y] ~ dnorm(0,tau.ranef)
+  }
+  
+})
+
+
+
+# bundle up data and constants
+constants <- list(nsizes = length(data$CW), # number of crabs
+                  year = data$year_index, # year index
+                  nyears = length(unique(data$year_index)), # number of years
+                  pi = pi # pi
+)
+data <- list(size = data$CW, # size captured
+             t = data$age # ages
+)
+
+inits_season <- list (size_inf = 100,
+                      k = 0.6,
+                      C = 0.7,
+                      ts = -0.6,
+                      t0 = 0,
+                      tau.y = 0.05,
+                      tau.ranef = 0.05,
+                      ranef = rep(0,length(unique(data_sub$year_index)))
+                      )
+
+
+cl <- makeCluster(4)
+
+set.seed(10120)
+
+clusterExport(cl, c("seasonal_growth", "inits_season", "data", "constants"))
+
+# Create a function with all the needed code
+out <- clusterEvalQ(cl, {
+  library(nimble)
+  library(coda)
+  
+  # build the model
+  model_season <- nimbleModel(seasonal_growth,constants,data,inits_season)
+  
+  # build the MCMC
+  mcmcConf_season <- configureMCMC(model_season,
+                                   monitors=c("size_inf", "k", "C","ts",
+                                              "t0","tau.y","tau.ranef"
+                                   ))
+  model_mcmc_season <- buildMCMC(mcmcConf_season)
+  
+  # compile the model and MCMC
+  # model
+  cmodel_season <- compileNimble(model_season)
+  # MCMC
+  cmodel_mcmc_season <- compileNimble(model_mcmc_season,project=model_season)
+  
+  cmodel_mcmc_season$run(100000,thin=10,
+                         reset=FALSE)
+  
+  return(as.mcmc(as.matrix(cmodel_mcmc_season$mvSamples)))
+})
+
+stopCluster(cl)
+
+# save samples
+saveRDS(out,'data/posterior_samples/savedsamples_growth.rds')
+
+out_sub <- list(out[[1]][100:10001,],out[[2]][100:10001,],
+                out[[3]][100:10001,],out[[4]][100:10001,])
+MCMCsummary(out_sub)
+
+# size_inf: mean 95.4, sd = 5.52
+# k: mean 0.67, sd 0.1
+
+#trace plot
+MCMCtrace(out_sub)
---
+++ data/model_data/README.md
@@ -0,0 +1,77 @@
+---
+editor_options: 
+  markdown: 
+    wrap: 72
+---
+
+**Description of model data files:**
+
+**Integral projection model data**
+
+*b*: boundary points of discretized crab size in integral projection
+model
+
+*y*: midpoint of discretized crab size of integral projection model
+
+**Time series data (D1)**
+
+*totalt*: total number of time points in each year *i* in time series
+data
+
+*totalo*: total number of observations (traps) in year *i*, at time *t*
+in time series data
+
+*soak_days*: number of soak days for each trap in time *t*, at trap *j*,
+at site *i* in time series data
+
+*f_index*: binary indicator of Fukui traps at time *t*, in trap *j*, at
+site *i* in time series data
+
+*m_index*: binary indicator of Minnow traps at time *t*, in trap *j*, at
+site *i* in time series data
+
+*s_index*: binary indicator of Shrimp traps at time *t*, in trap *j*, at
+site *i* in time series data
+
+*recruit_intro*: binary data structure indicating the time point when
+recruits enter the process model at site *i*, corresponding to t = 6
+
+*index_frac*: date index (i.e., D(t)) for each time *t* and site *i*,
+expressed as a fraction of a year (i.e., 0.5 = beginning of July)
+
+*n_cap*: total count of crabs removed, $n_{t,i,y}^R$, at time *t*, site
+*i*, size *y* in time series data
+
+*counts*: count of crabs removed, $n_{t,j,i,y}^C$, at time *t*, trap
+*j*, site *i*, size *y* in time series data
+
+
+**Size-at-age data (D2)**
+
+*growth_data*: size (CW), age, and year observed of each crab 
+
+
+**Batch mark-recapture data (D3)**
+
+*f_index_mc*: binary indicator of Fukui traps at obs *s* in
+mark-recapture data
+
+*m_index_mc*: binary indicator of Minnow traps at obs *s* in
+mark-recapture data
+
+*s_index_mc*: binary indicator of Shrimp traps at obs *s* in
+mark-recapture data
+
+*m2_mc*: count of recaptured marked crabs of size *y* in mark-recapture
+data
+
+*n1_mc*: count of marked crabs of size *y* in mark-recapture data
+
+*soak_days_mc*: number of soak days for each trap *s* in mark-recapture
+data
+
+*mc_index*: date index (i.e., D(t)) for the mark time point and
+recapture time point, expressed as a fraction of a year (i.e., 0.5 =
+beginning of July)
+
+*totalo_mc*: total number of observations (traps) in mark-recapture data
